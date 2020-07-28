e748b1a0391e5b029a76bec2be0c4771c8f46dbb
Mate Iszlai <iszimate@gmail.com>
2020-07-28 13:42:21 +0200

    Create Recipe class

---
+++ ReceptApp/Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReceptApp.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        
+        [ForeignKey(nameof(User))]
+        public int OwnerId { get; set; }
+        public string Name { get; set; }
+        public HashSet<Ingredient> Ingredients { get; private set; }
+        public string Description { get; set; }
+        public Time PreparationTime { get; set; }
+        public Time CookTime { get; set; }
+        public Time AdditionalTime { get; set; }
+        public int Servings { get; set; }
+        public HashSet<Picture> Pictures { get; set; }
+    }
+}
