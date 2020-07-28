b98838dc96b93eba078d0d5171fd29a2c3205a1a
Mate Iszlai <iszimate@gmail.com>
2020-07-28 10:28:15 +0200

    Create User class

---
+++ ReceptApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace ReceptApp.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public List<int> FavouriteRecipesIds { get; set; }
+    }
+}
