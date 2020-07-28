7777ee0333100cd3d6005dbfd7c1a95da93e1401
Mate Iszlai <iszimate@gmail.com>
2020-07-28 13:42:45 +0200

    Add recipes DbSet

--- ReceptApp/Models/RecipeDbContext.cs
+++ ReceptApp/Models/RecipeDbContext.cs
@@ -15,5 +15,6 @@ namespace ReceptApp.Models
         public DbSet<User> UserList { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
     }
 }
