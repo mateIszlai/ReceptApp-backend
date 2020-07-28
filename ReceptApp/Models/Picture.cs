bf1f2db9dec050e9c6456f5215b305c72db7c041
Mate Iszlai <iszimate@gmail.com>
2020-07-28 13:33:05 +0200

    Create Picture class

---
+++ ReceptApp/Models/Picture.cs
@@ -0,0 +1,9 @@
+﻿namespace ReceptApp.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
