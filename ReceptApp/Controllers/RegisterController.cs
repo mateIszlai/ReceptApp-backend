96c89f54a3cf43fb2810dc1ebf39a43943d8cf76
Mate Iszlai <iszimate@gmail.com>
2020-07-29 15:39:23 +0200

    Create registerController

---
+++ ReceptApp/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ReceptApp.Models;
+using ReceptApp.Models.Requests;
+
+namespace ReceptApp.Controllers
+{
+    [Route("api/register")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public RegisterController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
