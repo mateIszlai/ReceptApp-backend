dda03fc12bbffe2e19a6277b71081ce24eaadbba
Mate Iszlai <iszimate@gmail.com>
2020-07-30 11:36:24 +0200

    Create LogoutController

---
+++ ReceptApp/Controllers/LogoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ReceptApp.Models;
+
+namespace ReceptApp.Controllers
+{
+    [Route("api/logout")]
+    [ApiController]
+    public class LogoutController : ControllerBase
+    {
+        private readonly SignInManager<User> _signInManager;
+        
+        public LogoutController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+    }
+}
