dfb080a19b0ef3ffa0b6061ba4a8eaf8990577e8
Mate Iszlai <iszimate@gmail.com>
2020-07-30 11:18:50 +0200

    Add Login method

--- ReceptApp/Controllers/LoginController.cs
+++ ReceptApp/Controllers/LoginController.cs
@@ -21,5 +21,24 @@ namespace ReceptApp.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestData loginData)
+        {
+            var user = await _userManager.FindByNameAsync(loginData.UserName);
+
+            if(user == null)
+            {
+                return BadRequest("Wrong username or password");
+            }
+
+            if (await _userManager.CheckPasswordAsync(user, loginData.Password))
+            {
+                await _signInManager.SignInAsync(user, isPersistent: true);
+                return Ok(new[] { user.Id, user.UserName, user.Email });
+            }
+
+            return BadRequest("Wrong username or password");
+        }
     }
 }
