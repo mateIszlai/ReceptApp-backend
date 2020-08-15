37b49626c0fb81099e1d50e2834eaff9ab52db96
Máté Iszlai <iszimate@gmail.com>
2020-08-15 14:36:26 +0200

    Create UserToModify request helper class

---
+++ ReceptApp/Models/Requests/UserToModify.cs
@@ -0,0 +1,11 @@
+﻿namespace ReceptApp.Models.Requests
+{
+    public class UserToModify
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
