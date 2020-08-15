3a11740b670ae803c9a60cdc381c923dc848643c
Máté Iszlai <iszimate@gmail.com>
2020-08-15 17:44:49 +0200

    Create UserToSend request helper class

---
+++ ReceptApp/Models/Requests/UserToSend.cs
@@ -0,0 +1,18 @@
+﻿namespace ReceptApp.Models.Requests
+{
+    public class UserToSend
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string NickName { get; private set; }
+        public string Email { get; private set; }
+
+        public UserToSend(User user)
+        {
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            NickName = user.NickName;
+            Email = user.Email;
+        }
+    }
+}
