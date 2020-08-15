09ddba62eaae9424c0c13d5207622d422af5629d
Máté Iszlai <iszimate@gmail.com>
2020-08-15 14:53:34 +0200

    Remove password, add OldPassword and NewPassword property

--- ReceptApp/Models/Requests/UserToModify.cs
+++ ReceptApp/Models/Requests/UserToModify.cs
@@ -6,6 +6,7 @@
         public string LastName { get; set; }
         public string NickName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
     }
 }
