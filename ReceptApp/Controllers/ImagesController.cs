da1863a6812933d264871c092d7faf95ede5771c
Máté Iszlai <iszimate@gmail.com>
2020-08-26 10:01:44 +0200

    Create controller class for images

---
+++ ReceptApp/Controllers/ImagesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReceptApp.Models;
+
+namespace ReceptApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly RecipeDbContext _context;
+
+        public ImagesController(RecipeDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
