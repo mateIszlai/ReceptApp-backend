f8ea827476967d2dc4dced3a4c986d13108fe818
Máté Iszlai <iszimate@gmail.com>
2020-08-26 10:50:08 +0200

    Create PicturePostRequest request data class

---
+++ ReceptApp/Models/Requests/PicturePostRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ReceptApp.Models.Requests
+{
+    public class PicturePostRequest
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
