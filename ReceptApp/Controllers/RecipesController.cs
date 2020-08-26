e7b470aa387fd87f57fd6a2423da41acf89a7402
Máté Iszlai <iszimate@gmail.com>
2020-08-26 12:00:19 +0200

    Remove comment

--- ReceptApp/Controllers/RecipesController.cs
+++ ReceptApp/Controllers/RecipesController.cs
@@ -66,8 +66,6 @@ namespace ReceptApp.Controllers
         }
 
         // PUT: api/Recipes/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutRecipe(int id, RecipeToChange newRecipe)
         {
